5ba3389ba6023fb5f4f70d0c9b164af83081892c
cabralcodes <matheusaugustodev.web@gmail.com>
2026-05-20 17:34:28 -0300

    I was confused by an innerText bug, but my friends helped meI initialized the hike to the top.

---
+++ Javascript-roadmap/Projetos/calculator/index.js
@@ -0,0 +1,20 @@
+  // Variáveis
+const button = document.querySelectorAll(".btn");
+let conta = "";
+let Visor = document.getElementById("tela");
+Visor.innerText = conta;
+button.forEach(function(botoes){
+    botoes.addEventListener("click", function(){
+        console.log(this.value)
+        if(this.value === "="){
+            console.log(conta);
+            Visor.innerText = eval(conta);
+            //conta = Visor.innerText;
+            console.log(Visor);
+        }else {
+            conta+=  this.value;
+            Visor.innerText = conta;
+            Visor.innerText = conta.replace("*", "x");
+        }
+    })
+});
